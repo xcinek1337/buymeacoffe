b0ecf030bcd8382df9e11f8e27fd799470d719cb
Marcin <marcinchodkowski004@wp.pl>
2024-06-25 17:43:39 +0200

    added toast popup infomration about saved info profil

--- src/components/UploadButton.tsx
+++ src/components/UploadButton.tsx
@@ -1,27 +1,30 @@
 import { uploadToS3 } from '@/actions/uploadActions';
-import { faUpload } from '@fortawesome/free-solid-svg-icons';
+import { faPencil } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { ChangeEvent } from 'react';
 
 export default function UploadButton({ onUploadComplete }: { onUploadComplete: (url: string) => void }) {
-	async function upload(ev: ChangeEvent) {
+	async function upload(ev: ChangeEvent<HTMLInputElement>) {
 		const target = ev.target as HTMLInputElement;
 		if (target.files?.length) {
 			const file = target.files[0];
 			const formData = new FormData();
 			formData.set('file', file);
 			const result = await uploadToS3(formData);
-			onUploadComplete(result.url!);
+			onUploadComplete(result.url as string);
 		}
 	}
+
 	return (
-		<label className='cursor-pointer rounded-lg bg-gray-200 p-2'>
-			<FontAwesomeIcon icon={faUpload} />
-			<input
-				type='file'
-				className='hidden'
-				onChange={(ev) => upload(ev)}
-			/>
-		</label>
+		<>
+			<label className='bg-white shadow-sm shadow-black/30 p-2 cursor-pointer rounded-lg flex gap-1 items-center'>
+				<FontAwesomeIcon icon={faPencil} />
+				<input
+					className='hidden'
+					type='file'
+					onChange={(ev) => upload(ev)}
+				/>
+			</label>
+		</>
 	);
 }
