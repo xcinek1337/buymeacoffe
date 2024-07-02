9218af271271d1138b70939aa2ab9eed0166740e
Marcin <marcinchodkowski004@wp.pl>
2024-07-02 23:08:39 +0200

    added whole donation list in profile

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -2,6 +2,7 @@
 
 import ProfileInfoForm from '@/components/ProfileInfoForm';
 import { authOptions } from '@/lib/authOptions';
+import { Donation, DonationModel } from '@/models/Donation';
 import { ProfileInfoModel } from '@/models/ProfileInfo';
 import mongoose from 'mongoose';
 import { getServerSession } from 'next-auth';
@@ -16,10 +17,31 @@ export default async function ProfilePage() {
 	await mongoose.connect(process.env.MONGODB_URI!);
 	const profileInfoDoc = JSON.parse(JSON.stringify(await ProfileInfoModel.findOne({ email })))
 
+
+	let donationList: Donation[] = [];
+	if (profileInfoDoc) {
+		donationList = await DonationModel.find({
+			donateOwner: profileInfoDoc.email,
+			paid: true,
+		}).exec();
+	}
+
 	return (
 		<div className='max-w-2xl mx-auto px-4'>
 			<ProfileInfoForm profileInfo={profileInfoDoc} />
-			<div>donations list....</div>
+			<div>
+				<h2 className='w-full font-bold my-6 text-center'>Donation list</h2>
+				<ul className='flex flex-col gap-4'>
+					{donationList.map((d,i)=>{
+						return(
+							<li className='bg-white shadow-sm rounded-lg p-2' key={i}>
+							<h3><span className='font-bold'>{d.name}</span> send you {d.amount}$</h3>
+							<p className='font-mono'>{d.message}</p>
+							</li>
+						)
+					})}
+				</ul>
+			</div>
 		</div>
 	);
 }
